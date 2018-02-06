dcec8b393ad28a0f3ca34321bce3c5bc9ff8ffb9
Hoon Lee <yl.hoony@gmail.com>
2018-02-06 03:40:16 +0000

    automatically backed up by learn

--- app/controllers/store_admin_controller.rb
+++ app/controllers/store_admin_controller.rb
@@ -1,3 +1,12 @@
 class StoreAdminController < ApplicationController
 
+  def home
+
+  end
+
+
+  get 'admin/home', to: 'store_admin#home'
+  get 'admin/orders', to: 'store_admin#orders'
+  get 'admin/invoice', to: 'store_admin#invoice'
+
 end
